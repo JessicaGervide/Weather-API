15653fc9ff610143615d9c768263dfef3a336d7a
Jessica <jessica@gervide.se>
2022-08-02 20:22:44 +0200

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,4 @@
-// funktion som hämta data från Open Weather Map för positionen Vällingby
+// funktion som hämtar data från Open Weather Map för positionen Vällingby
 export function getWeather() {
   return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=59.367081&lang=se&lon=17.869143&appid=9df361c4e86e53ff51710451f6884f56&units=metric`)
   .then(response => response.json())
